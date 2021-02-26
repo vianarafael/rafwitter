473aa753ee80887ee6160dd1f7d81bb25fd50564
vianarafael <vianarafaelds@gmail.com>
2021-02-26 16:53:47 +0900

    Added the initial skeleton and the signup page

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' 
+
+import Signup from './pages/Signup'
+import Landing from './components/Landing'
+import { setContext } from 'apollo-link-context'
+
+const httpLink = new HttpLink({uri: 'http://localhost:4000'})
+const authLink = setContext(async(req, {headers}) => {
+  const token = localStorage.getItem('token')
+
+  return {
+    ...headers,
+    headers: {
+      Authorization: token ? `Bearer ${token}`: null
+    }
+  }
+})
+
+const link = authLink.concat(httpLink as any)
+const client = new ApolloClient({
+  link: (link as any),
+  cache: new InMemoryCache()
+})
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <p>Nooooo</p>
+          </Route>
+          <Route exact path="/landing">
+            <Landing />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
